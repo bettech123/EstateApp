dfe6219f7a2dabacd2b52d6b52c36d034abdbde5
bettech123 <bethel89ogu@gmail.com>
2020-06-09 15:22:29 +0100

    just minor typo

--- scr/EstateApp.web/Startup.cs
+++ scr/EstateApp.web/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Estate.Data.DatabaseContexts.AuthenticationDbContext;
 
 namespace EstateApp.web
 {
@@ -25,7 +26,7 @@ namespace EstateApp.web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContextPool<AuthenticationDbContext>(
-                options =>UseSqlServer(Configuration.GetConnectionStrings("AuthenticationConnection")))
+                options => options.UseSqlServer(Configuration.GetConnectionString("AuthenticationConnection")));
             services.AddControllersWithViews();
         }
 
