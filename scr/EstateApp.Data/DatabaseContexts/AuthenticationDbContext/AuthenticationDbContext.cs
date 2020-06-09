1995061bd56a1874ce1692a6ca43325686231817
bettech123 <bethel89ogu@gmail.com>
2020-06-09 12:44:31 +0100

    Added and updated authentication db context

--- scr/EstateApp.Data/DatabaseContexts/AuthenticationDbContext/AuthenticationDbContext.cs
+++ scr/EstateApp.Data/DatabaseContexts/AuthenticationDbContext/AuthenticationDbContext.cs
@@ -1,10 +1,17 @@
+
+using EstateApp.Data.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Estate.Data.DatabaseContexts.AuthenticationDbContext
 {
     public class AuthenticationDbContext : IdentityDbContext<ApplicationUser>
     {
+       public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options)
+            : base(options)
+            {
 
+            }
     }
 }
